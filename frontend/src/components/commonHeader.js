39e143880e1a649fd758828b4991317cbcfb350f
alice-shamaeva <alice.shamaeva@gmail.com>
2024-09-12 11:38:10 +0400

    Step 7

---
+++ frontend/src/components/commonHeader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { quit } from '../slices/authSlice.js';
+import routes from '../services/routes.js';
+
+const CommonHeader = () => {
+  const userData = useSelector((state) => state.user);
+  const isLoggedIn = userData.token !== '';
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const clickhandler = () => {
+    dispatch(quit());
+  };
+
+  return (
+    <Navbar className="shadow-sm bg white">
+      <Container>
+        <Navbar.Brand href={routes.home}>{t('header.brand')}</Navbar.Brand>
+        {isLoggedIn ? <Button onClick={clickhandler}>{t('header.button')}</Button> : null}
+      </Container>
+    </Navbar>
+  );
+};
+
+export default CommonHeader;
--- frontend/src/components/signUpForm.js
+++ frontend/src/components/signUpForm.js
@@ -9,50 +9,116 @@ import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
 import { useSignupMutation } from '../services/authApi';
-import validationSchema from '../services/validationSingUp';
-import CommonHeader from './CommonHeader';
+import validationSchema from '../services/validationSignUp.js';
+import CommonHeader from './commonHeader.js';
 import routes from '../services/routes';
 
 const signUpForm = () => {
+  const [singUp, {isLoading: isSingingUp}] = useSignupMutation();
+  const [errorUsername, setErrorUsername] = useState('');
+  const [focusUsername, setFocusedUsername] = useState(false);
+  const [focusPassword, setFocusedPassword] = useState(false);
+  const [focusConfirm, setFocusedConfirm] = useState(false);
+  const userData = useSelector((state) => state.user);
+  localStorage.setItem('token', userData.token);
+  const {t} = useTranslation();
+
   const formik = useFormik({
     initialValues: {
-      name: "",
-      email: "",
-      password: ""
+      username: '',
+      password: '',
+      confirmPassword: '',
     },
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
+    validationSchema,
+    onSubmit: async (values) => {
+      const {username, password} = values;
+
+      const response = await singUp({username, password});
+      if (Object.hasOwn(response, 'error')) {
+        if (response.error.status === 409) setErrorUsername(t('signUpForm.errors.alreadyExist'));
+      }
     },
   });
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        name="name"
-        type="name"
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="emaill"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
 
-      <button type="submit">Submit</button>
-    </form>
+  const focus = {
+    username: () => {
+      setFocusedUsername(true);
+      formik.validateField('username');
+    },
+    password: () => {
+      setFocusedPassword(true);
+      formik.validateField('password');
+    },
+    confirm: () => {
+      setFocusedConfirm(true);
+      formik.validateField('confirmPassword');
+    },
+  };
+
+  if (userData.token !== '') return <Navigate to={routes.home}/>;
+
+  return (
+    <>
+      <CommonHeader />
+      <Container fluid className="row justify-content-center align-content-center">
+        <Card className="h-50 w-25 m-3 mb-1 p-5 shadow">
+          <Card.Header as="h2" className="bg-white border-0 m-2 text-center">{t('signUpForm.title')}</Card.Header>
+          <Form onSubmit={formik.handleSubmit}>
+            <FloatingLabel className="mb-3" label={t('signUpForm.username')} controlId="username">
+              <Form.Control
+                type="name"
+                name="username"
+                className="w-100"
+                onBlur={focus.username}
+                isInvalid={focusUsername && (formik.errors.username || errorUsername)}
+                value={formik.values.username}
+                onChange={formik.handleChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errorUsername !== '' ? errorUsername : t(formik.errors.username)}
+              </Form.Control.Feedback>
+            </FloatingLabel>
+            <FloatingLabel className="mb-3" label={t('signUpForm.password')} controlId="password">
+              <Form.Control
+                type="password"
+                name="password"
+                className="w-100"
+                onBlur={focus.password}
+                isInvalid={focusPassword && formik.errors.password}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                {t(formik.errors.password)}
+              </Form.Control.Feedback>
+            </FloatingLabel>
+            <FloatingLabel className="mb-4" label={t('signUpForm.confirmPassword')} controlId="confirmPassword">
+              <Form.Control
+                type="password"
+                name="confirmPassword"
+                className="w-100"
+                onBlur={focus.confirm}
+                isInvalid={focusConfirm && formik.errors.confirmPassword}
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                {t(formik.errors.confirmPassword)}
+              </Form.Control.Feedback>
+            </FloatingLabel>
+            <Button
+              className="w-100 border-primary bg-white text-primary"
+              type="submit"
+              disabled={isSingingUp}
+            >
+              {t('signUpForm.button')}
+            </Button>
+          </Form>
+        </Card>
+
+      </Container>
+    </>
+
   );
 };
 
