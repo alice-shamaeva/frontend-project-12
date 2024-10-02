90ea8f011527012ce6ab0aeed5b79c60b7812dd1
alice-shamaeva <alice.shamaeva@gmail.com>
2024-10-02 20:45:30 +0400

    Stupid error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import LoginPage from './components/loginPage.js';
 import SignupPage from './components/signUpForm.js';
 import Chat from './components/chatPage/chatPage.js';
 import NotFoundPage from './components/notFoundPage.js';
-import { AuthProvider, useAuth } from './services/context/authContext.jsx';
+import { AuthProvider, useAuth } from './services/context/authContext.js';
 import routes from './services/routes.js';
 
 const LoggedInRoute = () => {
