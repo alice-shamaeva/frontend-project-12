e5325dc5b43066a804d48ee5eb20ffe93f430987
alice-shamaeva <alice.shamaeva@gmail.com>
2024-10-03 21:01:22 +0400

    Shit... 3

--- frontend/src/components/messages/MessagesForm.jsx
+++ frontend/src/components/messages/MessagesForm.jsx
@@ -1,67 +1,45 @@
 import { useSelector } from 'react-redux';
 import * as filter from 'leo-profanity';
 import Button from 'react-bootstrap/esm/Button';
-import Form from 'react-bootstrap/Form';
 import { Send } from 'react-bootstrap-icons';
+import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
-import * as Yup from 'yup';
 import { useAddMessageMutation } from '../../api/messages';
 
 const MessagesForm = () => {
+  const { t } = useTranslation();
   const currentChannelId = useSelector((state) => state.app.currentChannelId);
   const username = useSelector((state) => state.app.username);
   const [addMessage] = useAddMessageMutation();
-  const { t } = useTranslation();
-  const messageSchema = Yup.object().shape({
-    message: Yup.string().required(t('form.errors.required')),
-  });
-  const handleFormSubmit = async (values, { resetForm }) => {
-    const { message } = values;
-    const data = {
-      message: filter.clean(message),
-      channelId: currentChannelId,
-      username,
-    };
-    await addMessage(data).unwrap()
-      .then(() => {
-        resetForm();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const handleFormSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+      const { message } = values;
+      const data = {
+        message: filter.clean(message),
+        channelId: currentChannelId,
+        username,
+      };
+      await addMessage(data);
+      resetForm();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setSubmitting(false);
+    }
   };
-
   return (
     <div className="mt-auto py-3 px-5">
-      <Formik
-        initialValues={{ message: '' }}
-        onSubmit={handleFormSubmit}
-        validateOnChange={false}
-        validationSchema={messageSchema}
-      >
-        {({
-          handleSubmit, handleChange, values, errors,
-        }) => (
+      <Formik initialValues={{ message: '' }} onSubmit={handleFormSubmit}>
+        {({ handleSubmit, handleChange, values }) => (
           <Form onSubmit={handleSubmit}>
             <InputGroup>
-              <Form.Label htmlFor="newMessage" hidden>{t('form.labels.message')}</Form.Label>
-              <Form.Control
-                placeholder={t('form.placeholders.message')}
-                autoFocus
-                id="newMessage"
-                aria-label={t('form.placeholders.message')}
-                value={values.message}
-                onChange={handleChange}
-                isInvalid={!!errors.message}
-                type="text"
-                name="message"
-              />
+              <Form.Label htmlFor="new-message" hidden>{t('form.labels.message')}</Form.Label>
+              <Form.Control placeholder={t('form.placeholders.message')} autoFocus id="new-message" aria-label={t('form.labels.newMessage')} value={values.message} onChange={handleChange} type="text" name="message" />
               <Button type="submit">
                 <Send />
               </Button>
-              <Form.Control.Feedback type="invalid">{errors.message}</Form.Control.Feedback>
             </InputGroup>
           </Form>
         )}
